88917659ad879110fc882d4448c21272792beee8
Aya Jackson <pixelmixery@gmail.com>
2026-05-08 19:58:38 +1000

    docs: comments

--- app/middlewares/exception_middleware.py
+++ app/middlewares/exception_middleware.py
@@ -14,18 +14,43 @@ from ..settings import generate_settings
 
 class ExceptionMiddleware(BaseHTTPMiddleware):
     """
-    Middleware to handle exceptions globally.
+    File read and write combo for yaml sync to local instance
 
-
+    Attributes:
+        app (FastAPI): The base app through which we route exception messaging
+        settings (dict): Settings instance to load optional pushover notification
     """
 
     def __init__(self, app):
+        """
+        Loader for exception Middleware
+
+        Args:
+            app (FastAPI): app, from main, that exceptions pass through
+        """
+
         super().__init__(app)
         settings = generate_settings()
         # if not settings.config.local:
         #   self.pushover = PushoverUtils()
 
     async def dispatch(self, request, call_next):
+        """
+        Dispatches Error message through multiple channels
+
+        Args:
+            request (#TODO): I assume it's the next exception in the queue
+            callnext? (#TODO): I assume it's a... no clue
+             ():
+
+        Returns:
+            dict: error messaging
+        Raises:
+            Exception: Any error in the software
+            Exception(2): If there's an error with sending the error
+            Exception(3): Error with sending pushover notification of the error
+        """
+
         try:
             response = await call_next(request)
             return response
--- app/utils/helper.py
+++ app/utils/helper.py
@@ -4,15 +4,33 @@ import yaml
 
 
 class Helper:
+    """
+    Shared methods dump
+
+    Features:
+    - File read and write combo for json sync to local instance
+    """
+
     @staticmethod
     def read_write(path, method, data=None):
-        """File interaction central script"""
+        """
+        File read and write combo for yaml sync to local instance.
+
+        Args:
+            path (str): path to file to interact with.
+            method (str): "r" or "w" for with open.
+            data (dict): data to store into yaml.
+
+        Returns:
+            dict: from data if provided
+        """
+
         with open(path, method, encoding="utf-8") as f:
             if data:
                 f.write(yaml.safe_dump(data, sort_keys=False))
-            else:
-                return yaml.safe_load(f)
+            return yaml.safe_load(f)
 
+    # TODO: Move to anytype utils when ready
     def make_deeplink(self, space_id: str, object_id: str):
         """Builds deeplinks for link purposes"""
         return f"https://object.any.coop/{object_id}?spaceId={space_id}"
